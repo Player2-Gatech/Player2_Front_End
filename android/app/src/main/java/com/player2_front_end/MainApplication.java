6e2ca1fd75844194ba5f5db9e5db80aa53dcbcb6
Bryan Kim <bryan@lawn-128-61-19-85.lawn.gatech.edu>
2018-03-28 18:03:49 -0400

    pie chart works for IOS

--- android/app/src/main/java/com/player2_front_end/MainApplication.java
+++ android/app/src/main/java/com/player2_front_end/MainApplication.java
@@ -3,6 +3,9 @@ package com.player2_front_end;
 import android.app.Application;
 
 import com.facebook.react.ReactApplication;
+import com.oblador.vectoricons.VectorIconsPackage;
+import com.oblador.vectoricons.VectorIconsPackage;
+import com.oblador.vectoricons.VectorIconsPackage;
 import com.rnfs.RNFSPackage;
 import fr.bamlab.rnimageresizer.ImageResizerPackage;
 import com.imagepicker.ImagePickerPackage;
@@ -27,6 +30,9 @@ public class MainApplication extends Application implements ReactApplication {
     protected List<ReactPackage> getPackages() {
       return Arrays.<ReactPackage>asList(
           new MainReactPackage(),
+            new VectorIconsPackage(),
+            new VectorIconsPackage(),
+            new VectorIconsPackage(),
             new RNFSPackage(),
             new ImageResizerPackage(),
             new ImagePickerPackage(),
